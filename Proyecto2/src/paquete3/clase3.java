8ce755dbec5890fabf0b990cf0063f43125437a2
Diurno <Diurno@DESKTOP-A2FPPFB>
2024-01-12 10:45:23 +0100

    rama 3 commit

---
+++ Proyecto2/src/paquete3/clase3.java
@@ -0,0 +1,10 @@
+package paquete3;
+
+public class clase3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("mensaje de la rama 3");
+	}
+
+}
